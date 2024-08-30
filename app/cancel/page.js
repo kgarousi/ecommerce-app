93706345a07b3c5ff9c3f9e878584ca58061832b
kgarousi <kgarousi32@gmail.com>
2024-08-29 21:30:48 -0500

    add link to deployment

--- app/cancel/page.js
+++ app/cancel/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function CancelPage() {
     return (
         <div>
-            <div className="text-decoration-line: underline"><Link href="/"><span className="font-weight: 700;">Click here</span> to go back home</Link></div>
+            <div className="text-decoration-line: underline"><Link href="https://ecommerce-app-peach-ten.vercel.app/"><span className="font-weight: 700;">Click here</span> to go back home</Link></div>
         </div>
     )
 }
--- app/success/page.js
+++ app/success/page.js
@@ -4,7 +4,7 @@ export default function SuccessPage(){
     
         <div>
             <div>Order Complete!</div>
-            <div className="text-decoration-line: underline"><Link href="/"><span className="font-weight: 700;">Click here</span> to go back home</Link></div>
+            <div className="text-decoration-line: underline"><Link href="https://ecommerce-app-peach-ten.vercel.app/"><span className="font-weight: 700;">Click here</span> to go back home</Link></div>
         </div>
     )
 }
