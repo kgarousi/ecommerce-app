472230d0beead0d88aa1b58d341999b6202bd18f
kgarousi <kgarousi32@gmail.com>
2024-08-29 21:40:53 -0500

    update comments

--- app/product/page.js
+++ app/product/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import useCart from '../(store)/store'; // Adjust path if necessary
+import useCart from '../(store)/store'; 
 import { useRouter } from 'next/navigation';
 
 export default function ProductPage({ searchParams }) {
